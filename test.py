ec66397e9190d0296ffc5b017f359db145088d7d
Dave Brondsema <dbrondsema@slashdotmedia.com>
2025-08-20 17:22:02 -0400

    fix glob.magic_check on py3.13, experiment with stricter testing

--- test.py
+++ test.py
@@ -17,7 +17,25 @@ pathlib.Path('foo').glob('bar*')
 
 # issue #11
 assert glob.escape('ab*') == 'ab[*]'
+assert glob.escape(b'ab*') == b'ab[*]'
+
+import tokenize
+tokenize.blank_re.match(b'foo')
 
 # check for any other modules with original 're'
+bad_attrs = []
 for k, v in sys.modules.items():
     assert getattr(v, 're', None) in {None, regex}, f'{k} has reference to original `re`'
+
+"""
+    # this is quite aggressive, more than necessary I think
+    for attr in dir(v):
+        if attr == 'NOFLAG':
+            continue
+        attr_obj = getattr(v, attr)
+        # assert type(attr_obj).__module__ != 're', f'{k}.{attr} has reference to original `re`'
+        if type(attr_obj).__module__ == 're':
+            bad_attrs.append(f'{k}.{attr} ({type(attr_obj)})')
+
+assert not bad_attrs, bad_attrs
+"""
